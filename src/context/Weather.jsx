b886dd41b7f434a1f1175a8691cc387bac521070
Mustafa Yılmaz <105638480+MustafaYilmaz1625@users.noreply.github.com>
2023-03-05 01:50:38 +0300

    add weather.jsx

---
+++ src/context/Weather.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Card from "../components/Card";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import { WeatherProvider } from "./WeatherContext";
+
+function Weather() {
+  return (
+    <WeatherProvider>
+      <Header />
+      <div className="flex justify-center items-center mt-8">
+        <Card />
+      </div>
+      <Footer />
+    </WeatherProvider>
+  );
+}
+
+export default Weather;
