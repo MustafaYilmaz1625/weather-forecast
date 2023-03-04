c806dba74dc262e6a5baa31268b54d790ac47c77
Mustafa Yılmaz <105638480+MustafaYilmaz1625@users.noreply.github.com>
2023-03-05 01:51:35 +0300

    add header.jsx on page

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <h1 className='uppercase font-extrabold text-xl sm:text-2xl lg:text-3xl text-slate-200 text-center pt-[12%]'>
+        weather forecast
+      </h1>
+    </div>
+  );
+}
+
+export default Header;
