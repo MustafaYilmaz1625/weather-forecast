2483d5bf0dfe99848b4787e2ea4b67f4f87d20cb
Mustafa Yılmaz <105638480+MustafaYilmaz1625@users.noreply.github.com>
2023-03-05 01:51:00 +0300

    add weatherContext on page

---
+++ src/context/WeatherContext.jsx
@@ -0,0 +1,35 @@
+import { createContext, useState, useEffect } from "react";
+import axios from "axios";
+const WeatherContext = createContext();
+
+export function WeatherProvider({ children }) {
+  const [weather, setWeather] = useState();
+  const [location, setLocation] = useState("Ankara");
+
+  useEffect(() => {
+    const getWeatherData = async () => {
+      try {
+        const data = await axios(
+          `https://api.weatherbit.io/v2.0/forecast/daily?city=${`${location},tr`}&key=7dd8a708d1d348fdacf9bebe21501746&lang=tr`
+        );
+        setWeather(data.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getWeatherData();
+  }, [location]);
+
+  const values = {
+    weather,
+    setWeather,
+    location,
+    setLocation,
+  };
+
+  return (
+    <WeatherContext.Provider value={values}>{children}</WeatherContext.Provider>
+  );
+}
+
+export default WeatherContext;
