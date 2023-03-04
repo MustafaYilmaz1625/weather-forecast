2bc191dc7f71fe53cadb15b91a3dfcee0ae669d8
Mustafa Yılmaz <105638480+MustafaYilmaz1625@users.noreply.github.com>
2023-03-05 01:51:15 +0300

    add card.jsx on page

---
+++ src/components/Card.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Forecast from "./Forecast";
+import Location from "./Location";
+
+function Card() {
+  return (
+    <div className='border-2 border-solid border-slate-600 rounded-xl w-auto bg-slate-700 m-10 mb-0 mt-3 opacity-80 panel'>
+      <Location />
+      <Forecast />
+    </div>
+  );
+}
+
+export default Card;
