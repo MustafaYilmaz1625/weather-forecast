95edea8fc0644a6e813094e9270f61320ebe5afe
Mustafa Yılmaz <105638480+MustafaYilmaz1625@users.noreply.github.com>
2023-03-10 23:31:53 +0300

    fix error

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
   return (
     <div>
       <h1 className='uppercase font-extrabold text-xl sm:text-2xl lg:text-3xl text-slate-200 text-center pt-[12%]'>
-        weather forecast
+        weather
       </h1>
     </div>
   );
