1a0321346f83965c19c8e064d81d98869e369dc2
Mustafa Yılmaz <105638480+MustafaYilmaz1625@users.noreply.github.com>
2023-03-10 23:28:17 +0300

    add github.linkdin and gmail  are acounts

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,23 +4,23 @@ import { HiOutlineMail } from "react-icons/hi";
 
 function Footer() {
   return (
-    <div className='flex justify-center items-center'>
-      <div className='text-white mb-20 w-60 sm:w-72 h-16 flex justify-around items-center border-x-2 border-b-2 rounded-b-lg bg-blue-800'>
+    <div className="flex justify-center items-center">
+      <div className="text-white bg-gray-400 mb-20 w-60 sm:w-72 h-16 flex justify-around items-center border-x-2 border-b-2 rounded-b-lg ">
         <a
-          href='https://www.linkedin.com/in/unaldutlu/'
-          className='rounded-3xl bg-red-700 hover:bg-red-800 p-2'
+          href="https://www.linkedin.com/in/mustafa-y%C4%B1lmaz-4b196023a/"
+          className="rounded-3xl bg-blue-600 hover:bg-blue-800 p-2"
         >
           <FaLinkedin size={25} />
         </a>
         <a
-          href='https://github.com/unaldutlu'
-          className='rounded-3xl bg-red-700 hover:bg-red-800 p-2'
+          href="https://github.com/MustafaYilmaz1625"
+          className="rounded-3xl bg-gray-700 hover:bg-gray-600 p-2"
         >
           <FaGithub size={25} />
         </a>
         <a
-          href='https://mail.google.com/mail/u/0/?hl=tr&tf=cm&fs=1&to=unaldutlu@gmail.com'
-          className='rounded-3xl bg-red-700 hover:bg-red-800 p-2'
+          href="https://mail.google.com/mail/u/0/?hl=tr&tf=cm&fs=1&to=myyilmaz1625@gmail.com"
+          className="rounded-3xl bg-zinc-500 hover:bg-gray-600 p-2"
         >
           <HiOutlineMail size={25} />
         </a>
